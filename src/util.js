eed1172c7265bd51067715439aced6693526592b
Tim DuBois <tim@neophilus.net>
2018-03-23 07:51:58 +0100

    Remove password polyfill, we will never use it here.

--- src/util.js
+++ src/util.js
@@ -363,78 +363,11 @@
 
             });
 
-        // Password.
-        $this.find('input[type=password]')
-            .each(function() {
-
-                var i = $(this);
-                var x = $(
-                    $('<div>')
-                    .append(i.clone())
-                    .remove()
-                    .html()
-                    .replace(/type="password"/i, 'type="text"')
-                    .replace(/type=password/i, 'type=text')
-                );
-
-                if (i.attr('id') != '')
-                    x.attr('id', i.attr('id') + '-polyfill-field');
-
-                if (i.attr('name') != '')
-                    x.attr('name', i.attr('name') + '-polyfill-field');
-
-                x.addClass('polyfill-placeholder')
-                    .val(x.attr('placeholder')).insertAfter(i);
-
-                if (i.val() == '')
-                    i.hide();
-                else
-                    x.hide();
-
-                i
-                    .on('blur', function(event) {
-
-                        event.preventDefault();
-
-                        var x = i.parent().find('input[name=' + i.attr('name') + '-polyfill-field]');
-
-                        if (i.val() == '') {
-
-                            i.hide();
-                            x.show();
-
-                        }
-
-                    });
-
-                x
-                    .on('focus', function(event) {
-
-                        event.preventDefault();
-
-                        var i = x.parent().find('input[name=' + x.attr('name').replace('-polyfill-field', '') + ']');
-
-                        x.hide();
-
-                        i
-                            .show()
-                            .focus();
-
-                    })
-                    .on('keypress', function(event) {
-
-                        event.preventDefault();
-                        x.val('');
-
-                    });
-
-            });
-
         // Events.
         $this
             .on('submit', function() {
 
-                $this.find('input[type=text],input[type=password],textarea')
+                $this.find('input[type=text],textarea')
                     .each(function(event) {
 
                         var i = $(this);
@@ -472,23 +405,6 @@
                             case 'submit':
                             case 'reset':
                                 break;
-
-                            case 'password':
-                                i.val(i.attr('defaultValue'));
-
-                                x = i.parent().find('input[name=' + i.attr('name') + '-polyfill-field]');
-
-                                if (i.val() == '') {
-                                    i.hide();
-                                    x.show();
-                                }
-                                else {
-                                    i.show();
-                                    x.hide();
-                                }
-
-                                break;
-
                             case 'checkbox':
                             case 'radio':
                                 i.attr('checked', i.attr('defaultValue'));
